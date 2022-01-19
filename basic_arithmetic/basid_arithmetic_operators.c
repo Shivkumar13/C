4b96fcdeaa7affa2363d7bb0701f5739deafaab6
shivkumar13 <shivkumar.ople97@gmail.com>
2022-01-20 03:35:07 +0530

    Needs update for Basic_Operator.c

---
+++ basic_arithmetic/basid_arithmetic_operators.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+
+int num_1;
+int num_2;
+int summation;
+int subtraction;
+int multiplication;
+int quotient;
+int remainder;
+
+
+int main(void)
+{
+
+
+    //Needs Update
+    
+}
