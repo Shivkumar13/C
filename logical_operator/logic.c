441c447326de42484a57c9efaefa4d468cb308b2
shivkumar13 <shivkumar.ople97@gmail.com>
2022-01-21 19:27:53 +0530

    Adding logic operators

---
+++ logical_operator/logic.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int num1;
+int num2;
+int result;
+
+
+int main(void)
+{
+
+printf("Enter value for num1:");
+scanf("%d", &num1);
+
+printf("Enter value for num2:");
+scanf("%d", &num2);
+
+result = (num1 > num2);
+printf("result of num1 > num2 = %d\n", result);
+
+result = (num1 >= num2);
+printf("result of num1 >= num2 = %d\n", result);
+
+result = (num1 < num2);
+printf("result of num1 < num2 = %d\n", result);
+
+result = (num1 <= num2);
+printf("result of num1 <= num2 = %d\n", result);
+
+result = (num1 == num2);
+printf("result of num1 == num2 = %d\n", result);
+
+result = (num1 != num2);
+printf("result of num1 != num2 = %d\n", result);
+
+exit(0);
+}
