365fce5c6cb0f2b872d3ad525e7381c0c88e3e4a
shivkumar13 <shivkumar.ople97@gmail.com>
2022-01-23 23:59:54 +0530

    Conditional Theory

---
+++ Negation Operator/negation_2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int n;
+int m;
+
+int main(void)
+{
+
+    puts("In all cases m is assigned to !n");
+    n = 100;
+    m = !n;
+    printf("(n is positive):n=%d, m =%d\n",n,m);
+
+    n = 1;
+    m = !n;
+    printf("(n is one):n =%d, m=%d\n", n , m);
+
+    //Note: In negation, only negation of 0 comes as 1, otherwise every positive, negative integers
+    n = 0;
+    m = !n;
+    printf("(n is zero):n=%d, m=%d\n", n, m);
+
+    n = -100;
+    m = !n;
+    printf("(n is negative):n=%d, m=%d\n", n, m);
+
+   
+    n = 2200;
+    m = !n;
+    printf("(n is positive):n=%d, m=%d\n", n, m);
+
+    n = -100.009;
+    m = !n;             //Make sure you are giving type specifier correctly
+    printf("(n is negative):n=%d, m=%d\n", n, m);
+
+
+    n = 2200;
+    m = !n;
+    printf("(n is positive):n=%d, m=%d\n", n, m);   
+
+    exit(0);
+
+}
