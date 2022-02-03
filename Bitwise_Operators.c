e0ad0e1c69a2fc09ca375e8f7ca502297db13e22
shivkumar13 <shivkumar.ople97@gmail.com>
2022-02-04 04:26:50 +0530

    Adding some theory

--- Bitwise_Operators.c
+++ Bitwise_Operators.c
@@ -10,9 +10,9 @@ int main(void)
 
     puts("Bitwise NOT:");
     rs = ~n1;
-    printf("n1 = %d, n2= %d, rs=%d\n", n1, n2, rs);
+    printf("n1 = %d, ~n1= %d\n", n1, rs);
     rs = ~n2;
-    printf("n1 = %d, n2= %d, rs=%d\n", n1, n2, rs);
+    printf("n2 = %d, ~n2= %d\n", n2, rs);
 
     puts("Bitwise OR");
     rs = n1 | n2;
