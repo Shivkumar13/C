595612344c2603c6c53c723ae22756dfb8c086d0
shivkumar13 <shivkumar.ople97@gmail.com>
2022-03-23 05:17:13 +0530

    Pointer arithmetic of struct

---
+++ Address_Arithmetic/ptr_arithmetic_struct_1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct test_addr
+{
+    int i;
+    float f;
+    char c;
+};
+
+int main(void)
+{
+
+    struct test_addr inA;
+    struct test_addr* ptr;
+
+    ptr = (struct test_addr*)1000;
+
+    printf("Size of struct = %llu\n", sizeof(struct test_addr));
+
+    for(int i=0; i < 5; ++i)
+    {
+        printf("Address arithmetic of structure is %llu\n", (unsigned long long int)(ptr+i));
+    }
+
+    exit(0);
+
+}
