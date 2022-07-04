8cd37f880b9e18428e750294decfb590764ce838
Shivkumar13 <shivkumar.ople97@gmail.com>
2022-07-04 20:31:46 +0530

    committing 7.13.c

---
+++ Exercise_7B/7.13.c
@@ -0,0 +1,210 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int i, j;
+    for(i = 0; i < 8; ++i)
+        for(j = i; j < 8; ++j)
+            printf("i = %d, j = %d\n", i, j);
+    exit(0);
+}
+
+/*
+Track i = 0
+    checking i < 8: TRUE
+        j = i 
+        Track j = 0
+            checking j < 8: TRUE
+                i = 0, j = 0
+        ++j
+        Track j = 1
+            checking j < 8: TRUE
+                i = 0, j = 1
+        ++j
+        Track j = 2
+            checking j < 8: TRUE
+                i = 0, j = 2
+        ++j
+        Track j = 3
+            checking j < 8: TRUE
+                i = 0, j = 3
+        ++j
+        Track j = 4
+            checking j < 8: TRUE
+                i = 0, j = 4
+        ++j
+        Track j = 5
+            checking j < 8: TRUE
+                i = 0, j = 5
+        ++j
+        Track j = 6
+            checking j < 8: TRUE
+                i = 0, j = 6
+        ++j
+        Track j = 7
+            checking j < 8: TRUE
+                i = 0 , j = 7
+        ++j
+        Track j = 8
+            checking j < 8: FALSE
+++i 
+    Track i = 1
+         checking i < 8 : TRUE
+        j = i
+         Track j = 1
+            checking j < 8: TRUE
+                i = 1, j = 1
+        ++j
+         Track j = 2
+            checking j < 8: TRUE
+                i = 1, j = 2
+        ++j
+         Track j = 3
+            checking j < 8: TRUE
+                i = 1, j = 3
+        ++j
+        Track j = 4 
+            checking j < 8: TRUE
+                i = 1, j = 4
+        ++j
+        Track j = 5
+            checking j < 8: TRUE
+                i = 1, j = 5
+        ++j
+        Track j = 6
+            checking j < 8: TRUE
+                i = 1, j = 6
+        ++j 
+        Track j = 7
+            checking j < 8: TRUE
+                i = 1, j = 7
+        ++j
+        Track j = 8:
+            checking j < 8: FALSE
+
+++i : i = 1 + 1: i = 2
+    checking i < 8: TRUE
+        j = i
+         Track j = 2
+            checking j < 8: TRUE
+                i = 2, j = 2
+        ++j
+         Track j = 3
+            checking j < 8: TRUE
+                i = 2, j = 3
+        ++j
+         Track j = 4
+            checking j < 8: TRUE
+                i = 2, j = 4
+        ++j
+         Track j = 5
+            checking j < 8: TRUE
+                i = 2, j = 5
+        ++j
+         Track j = 6
+            checking j < 8: TRUE
+                i = 2, j = 6
+        ++j
+         Track j = 7
+            checking j < 8: TRUE
+                i = 2, j = 7
+        ++j
+         Track j = 8:
+            checking j < 8 : FALSE
+
+++i : i = i + 1: i = 3
+        checking i < 8: TRUE
+            j = i
+        Track j = 3
+            cheking j < 8: TRUE
+                i = 3, j = 3
+        ++j
+        Track j = 4
+            checking j < 8: TRUE
+                i = 3, j = 4
+        ++j
+        Track j = 5
+            checking j < 8: TRUE
+                i = 3, j = 5
+        ++j
+        Track j = 6
+            checking j < 8: TRUE
+                i = 3, j = 6
+        ++j
+        Track j = 7
+            checking j < 8: TRUE
+                i = 3, j = 7
+        ++j
+        Track j = 8
+            checking j < 8: FALSE
+    
+++i : i = 4
+        checking i < 8: TRUE
+            j = i
+        Track j = 4
+            checking j < 8: TRUE
+                i = 4, j = 4
+        ++j
+        Track j = 5
+            checking j < 8: TRUE
+                i = 4, j = 5
+        ++j
+        Track j = 6
+            checking j < 8: TRUE
+                i = 4, j = 6
+        ++j
+        Track j = 7
+            checking j < 8: TRUE
+                i = 4, j = 7
+        ++j
+        Track j = 8
+            checking j < 8: FALSE
+
+++i : i = 5
+    checking i < 8: TRUE
+        j = i
+        Track j = 5
+            checking j < 8: TRUE
+                i = 5, j = 5
+        ++j
+        Track j = 6
+            checking j < 8: TRUE   
+                i = 5, j = 6
+        ++j
+        Track j = 7
+            checking j < 8: TRUE
+                i = 5, j = 7
+        ++j
+        Track j = 8:
+            checking j < 8: FALSE
+            skip
+
+++i : i = 6
+    checking i < 8: TRUE
+        j = i
+        Track j = 6
+            checking j < 8: TRUE
+                i = 6, j = 6
+        ++j
+        Track j = 7
+            checking j < 8: TRUE
+                i = 6, j = 7
+        ++j
+         Track j = 8
+            checking j < 8: FALSE
+
+++i: i = 7
+    checking i < 8: TRUE
+        j = i
+        Track j = 7
+            checking j < 8: TRUE
+            i = 7, j = 7
+        ++j
+        Track j = 8
+            checking j < 8: FALSE
+
+++i: i = 8
+    checking i < 8: FALSE
+    Skip
+*/
