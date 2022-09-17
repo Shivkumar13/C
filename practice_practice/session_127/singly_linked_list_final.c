903d98fb4fee6c2f4e399d89717a8095e9962a32
Shivkumar13 <sople@redhat.com>
2022-09-17 07:29:29 +0530

    Singly Linked List Final

--- practice_practice/session_127/singly_linked_list_final.c
+++ practice_practice/session_127/singly_linked_list_final.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <assert.h>
 
+/*
+https://stackoverflow.com/questions/56317418/malloc-corrupted-top-size
+*/
+
 #define SUCCESS 1
 #define FAILURE 2
 #define LIST_DATA_NOT_FOUND 3
@@ -59,12 +63,16 @@ int destroy_list(struct node* p_list);
 
 void test_suite(void);
 void test_suite2(void);
-
+void test_suite3(void);
+void test_suite4(void);
 
 int main(void)
 {
-    //test_suite();
+    
+    test_suite();
     test_suite2();
+    test_suite3();
+    //test_suite4();
 
     return (0);
 }
@@ -382,19 +390,271 @@ int concat_lists(struct node* p_list_1, struct node* p_list_2,
                     struct node** pp_concat_list)
 {
     struct node* p_run = NULL;
+    struct node* p_run2 = NULL;
+    struct node* p_concat_list = NULL;
+    int ret;
+
+    p_concat_list = create_list(); 
+
+    p_run = p_list_1->next;
+    while(p_run != NULL)
+    {
+        ret = insert_at_end(p_concat_list, p_run->data);
+        assert(ret == SUCCESS);
+        p_run = p_run->next;
+    }
+       
+    p_run2 = p_list_2->next;  
+    while(p_run2 != NULL)
+    {
+        ret = insert_at_end(p_concat_list, p_run2->data);
+        assert(ret == SUCCESS);
+        p_run2 = p_run2->next;
+    }
+
+    *pp_concat_list = p_concat_list;
+
+    return (SUCCESS);
+}
+
+int append_list(struct node* p_list_1, struct node* p_list_2)
+{
+    struct node* p_run = NULL;
 
     p_run = p_list_1;
     while(p_run->next != NULL)
     {
-
         p_run = p_run->next;
     }
 
-    p_run->next = p_list_2->next;
-
-    *pp_concat_list = p_list_1;
+    p_run->next = p_list_2->next;  
 
     return (SUCCESS);
+}
+
+int merge_lists(struct node* p_list_1, struct node* p_list_2, 
+                struct node** pp_merged_list)
+{
+    struct node* p_run1 = NULL;
+    struct node* p_run2 = NULL;
+    struct node* p_merged_list = NULL;
+    int status;
+    int data;
+
+    p_merged_list = create_list();
+
+    p_run1 = p_list_1->next;
+    p_run2 = p_list_2->next;
+
+    while(1)
+    {
+        if(p_run1 == NULL)
+        {
+            while(p_run2 != NULL)
+            {
+                status = insert_at_end(p_merged_list, p_run2->data);
+                assert(status == SUCCESS);
+                p_run2 = p_run2->next;
+            }
+            
+            break;
+        }
+        
+        if(p_run2 == NULL)
+        {
+            while(p_run1 != NULL)
+            {
+                status = insert_at_end(p_merged_list, p_run1->data);
+                assert(status == SUCCESS);
+                p_run1 = p_run1->next;
+            }
+            break;
+        }
+        
+        if(p_run1->data <= p_run2->data)
+        {
+            data = p_run1->data;
+            p_run1 = p_run1->next;
+        }
+
+        else
+        {
+            data = p_run2->data;
+            p_run2 = p_run2->next;
+        }     
+           
+        status = insert_at_end(p_merged_list, data);
+        assert(status == SUCCESS);
+    }
+   
+    *pp_merged_list = p_merged_list;
+     return(SUCCESS);
+}
+
+
+int to_array(struct node* p_list_1, int** pp_arr, int* p_size)
+{
+    struct node* p_run = NULL;
+    int* p_arr = NULL;
+    int i;
+
+    p_arr = (int*)malloc(*p_size * sizeof(int));
+    assert(p_arr != NULL);
+
+    i = 0;
+    p_run = p_list_1->next;
+    while(p_run != NULL)
+    {
+        p_arr[i] = p_run->data;
+        i++;
+        p_run = p_run->next;
+    }
+    
+    *pp_arr = p_arr;
+    *p_size = i;
+    return(SUCCESS);
+}
+
+
+int to_list(int* p_arr, int N, struct node** pp_list)
+{
+    struct node* p_list = NULL;
+    int ret;
+
+    p_list = create_list();
+ 
+    int i = 0;
+
+    for(i = 0; i < N; ++i)
+    {
+        ret = insert_at_end(p_list, p_arr[i]);
+        assert(ret == SUCCESS);
+    }
+
+    *pp_list = p_list;
+    return(SUCCESS);
+}
+
+
+
+int get_reversed_list(struct node* p_list, struct node** pp_reversed_list)
+{
+    struct node* p_rev_list = NULL;
+    struct node* p_run = NULL;
+    int ret ;
+
+    p_rev_list = create_list();
+
+    p_run = p_list->next;
+    while(p_run != NULL)
+    {
+        ret = insert_at_start(p_rev_list, p_run->data);
+        assert(ret == SUCCESS);
+        p_run = p_run->next;
+    }
+
+    *pp_reversed_list = p_rev_list;
+    return(SUCCESS);
+}
+
+/*
+int reverse_list(struct node* p_list)
+{
+
+
+    HEAD -> 1st -> 2nd -> 3rd -> 4th -> 5th -> NULL
+
+    NULL <- 1st <- 2nd <- 3rd <- 4th <- 5th <- HEAD
+
+    int count, i;
+    struct node* p_run = NULL;
+    struct node* temp_node = NULL;
+    struct node* res_head_node = NULL;
+
+    res_head_node = p_list;
+
+    temp_node = p_list->next;
+
+
+    temp_node->next = NULL;
+/*
+    struct node* p_run = NULL;
+    p_run = p_list;
+    while(p_run->next != NULL)
+    {
+        p_list = p_list->next;
+    }
+
+    int ret;
+    struct node* p_run = NULL;
+    p_run = p_list;
+    while(p_run->next != NULL)
+    {
+        ret = insert_at_start(p_list, p_run->data);
+        assert(ret == SUCCESS);
+        p_run = p_run->next;
+    }
+
+    p_list = p_run;
+    return(SUCCESS);
+
+}
+*/
+
+/*
+void test_suite4()
+{
+    struct node* p_list = NULL;
+    int i = 0;
+    int ret;
+
+    p_list = create_list();
+
+    for(i = 0; i < 10; ++i)
+    {   
+        ret = insert_at_end(p_list, (i+1) * 10);
+        assert(ret == SUCCESS);
+    }
+
+    show_list(p_list, "Original List, before reversing original:");
+
+    reverse_list(p_list);
+
+    show_list(p_list, "Reversed List in the Original List itself:");
+
+
+    destroy_list(p_list);
+    p_list = NULL;
+    
+}
+*/
+
+void test_suite3()
+{
+    struct node* p_list = NULL;
+    struct node* p_get_rev_list = NULL;
+    int ret;
+    int i;
+
+    p_list = create_list();
+
+    for(i = 0; i < 5; ++i)
+    {
+        ret = insert_at_end(p_list, (i+1)*1000);
+        assert(ret == SUCCESS);
+    }
+
+    show_list(p_list, "original list");
+
+    ret = get_reversed_list(p_list, &p_get_rev_list);
+    assert(ret == SUCCESS);
+
+    show_list(p_get_rev_list, "reversed list");
+
+    destroy_list(p_list);
+    destroy_list(p_get_rev_list);
+    p_list = NULL;
+    p_get_rev_list = NULL;
 }
 
 void test_suite2(void)
@@ -407,7 +667,7 @@ void test_suite2(void)
 
     p_list_1 = create_list();
     p_list_2 = create_list();
-
+    
     for(i = 0; i < 5; ++i)
     {
         ret = insert_at_end(p_list_1, (i+1) * 10);
@@ -426,10 +686,84 @@ void test_suite2(void)
     assert(ret == SUCCESS);
     show_list(p_concat_list, "Showing Concatenated List");
 
+    ret = append_list(p_list_1, p_list_2);
+    assert(ret == SUCCESS);
+    show_list(p_list_1, "Showing Append List");
+
+
+    struct node* p_merge_list_input = NULL;
+    struct node* p_merge_list_input_2 = NULL;
+    struct node* p_merged_list = NULL;
+
+    p_merge_list_input = create_list();
+    p_merge_list_input_2 = create_list();
+
+     for(i = 0; i < 5; ++i)
+    {
+        ret = insert_at_end(p_merge_list_input, (i+1) * 2);
+        assert(ret == SUCCESS);
+    }
+    show_list(p_merge_list_input, "Showing Merge List Input 1");
+
+    for(i = 0; i < 5; ++i)
+    {
+        ret = insert_at_end(p_merge_list_input_2, (i+6) * 5);
+        assert(ret == SUCCESS);
+    }
+    show_list(p_merge_list_input_2, "Showing Merge List Input 2");
+
+
+    ret = merge_lists(p_merge_list_input, p_merge_list_input_2, &p_merged_list);
+    assert(ret == SUCCESS);
+
+    show_list(p_merged_list, "merged list");
+
+
+    struct node* p_arr_list = NULL;
+    p_arr_list = create_list();
+
+    for(i = 0; i < 5; ++i)
+    {
+        ret = insert_at_end(p_arr_list, (i+1) * 10);
+        assert(ret == SUCCESS);
+    }
+    show_list(p_arr_list, "Showing Array List:");
+
+    int* p_arr = NULL;
+    int size = 0;
+    ret = to_array(p_arr_list, &p_arr, &size);
+    assert(ret == SUCCESS);
+    
+    for(i = 0; i < size; ++i)
+        printf("Showing Array: arr[%d]=%d\n", i, p_arr[i]);
+    
+    int nr_of_element = 10;
+    struct node* p_list_tolist = NULL;
+
+    p_list_tolist = create_list();
+
+    p_arr = (int*)calloc(nr_of_element, sizeof(int));
+    assert(p_arr != NULL);
+
+    for(i = 0; i < nr_of_element; ++i)
+        p_arr[i] = (i+1)*100;
+    
+    ret = to_list(p_arr, nr_of_element, &p_list_tolist);
+    assert(ret == SUCCESS);
+
+    show_list(p_list_tolist, "Showing p_list_tolist");
+    
     destroy_list(p_list_1);
+    destroy_list(p_list_tolist);
+    p_list_1 = NULL;
+    p_list_tolist = NULL;
+    free(p_arr);
+    p_arr = NULL;
 
+   destroy_list(p_merged_list);
+   p_merged_list = NULL;
+    
 }
-
 
 void test_suite(void)
 {
@@ -438,7 +772,7 @@ void test_suite(void)
     int ret;
     int data;
 
-     p_list = create_list();
+    p_list = create_list();
 
     for(i = 0; i < 5; ++i)
      {
